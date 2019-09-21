ddbe7f40e29f00005bbcf22fc991482fcdc24ccf
Darius Tsafack <clintontsafack@gmail.com>
2019-09-21 12:55:15 +0100

    Updates

---
+++ precache-manifest.25aca0df4b603ec5e427533cd6d1ce38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5b9facb8669052a1a0930d159edf782",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "43efa6439901e3c3a3eb",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "96f062df911bfc695efc",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "43efa6439901e3c3a3eb",
+    "url": "/contactmanager/static/js/2.dbdf1af9.chunk.js"
+  },
+  {
+    "revision": "96f062df911bfc695efc",
+    "url": "/contactmanager/static/js/main.60b7877c.chunk.js"
+  },
+  {
+    "revision": "b6597f31777016aa759a",
+    "url": "/contactmanager/static/js/runtime~main.77e22976.js"
+  }
+]);
